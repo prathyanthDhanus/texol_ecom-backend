a21be7d7cea5bebf210212917af431c576a149e9
prathyanthDhanus <prathyanthbusinessmail@gmail.com>
2025-08-12 20:56:25 +0530

    feat(server) : complete the server setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
 
 const app: Application = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.get("/", (req: Request, res: Response) => {
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
-// src/server.ts
+import dotenv from "dotenv";
+dotenv.config();
+
+import mongoose from "mongoose";
 import app from "./app";
 
+const url = process.env.MONGODB_URL as string;
 const PORT = process.env.PORT || 3000;
+
+// MongoDB connection setup
+mongoose.connect(url)
+  .then(() => console.log("Mongodb atlas connected"))
+  .catch((error:string) => console.log("Error:", error));
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
